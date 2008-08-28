7e4eb371fe4333799b9ad763170679d4e56b242a
Campbell Barton <ideasman42@gmail.com>
2008-08-28 23:12:29 +0000

    svn merge -r 16287:HEAD https://svn.blender.org/svnroot/bf-blender/trunk/blender still getting errors compiling blenderplayer

--- source/blender/src/space.c
+++ source/blender/src/space.c
@@ -1976,7 +1976,7 @@ static void winqreadview3dspace(ScrArea *sa, void *spacedata, BWinEvent *evt)
 						G.vd->drawtype= pupval;
 						doredraw= 1;
 					}
-                                }
+                }
 				
 				break;
 			case EKEY:
