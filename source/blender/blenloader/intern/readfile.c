cc3504505c92401ef7faf01894aab355e6399f76
Jason Wilkins <Jason.A.Wilkins@gmail.com>
2010-06-04 02:34:02 +0000

    Misplaced curly brace. Replaced BRUSH_BIT macro because my prefered name BIT was taken.

--- source/blender/blenloader/intern/readfile.c
+++ source/blender/blenloader/intern/readfile.c
@@ -10914,7 +10914,6 @@ static void do_versions(FileData *fd, Library *lib, Main *main)
 		for (brush= main->brush.first; brush; brush= brush->id.next)
 			if(brush->detail == 0.0f)
 				brush->detail = 0.25f;
-	}
 		if (brush->smoothness == 0.0f)
 			brush->smoothness = 0.25f;
 	
