60f19c526fa9fd1bf7dfe67364a31c57286093b4
Jeroen Bakker <j.bakker@atmind.nl>
2012-04-23 10:48:51 +0000

    TileBranch  * Fix for bug [#31031] Blender chrash after disconnect Hue Saturation Value node from image.
    removed the is connected check. this could result is a wrongly formed operation tree

--- source/blender/compositor/nodes/COM_HueSaturationValueNode.cpp
+++ source/blender/compositor/nodes/COM_HueSaturationValueNode.cpp
@@ -36,35 +36,33 @@ HueSaturationValueNode::HueSaturationValueNode(bNode *editorNode): Node(editorNo
 }
 
 void HueSaturationValueNode::convertToOperations(ExecutionSystem *graph, CompositorContext * context) {
-    InputSocket *valueSocket = this->getInputSocket(0);
-    InputSocket *colourSocket = this->getInputSocket(1);
-    OutputSocket *outputSocket = this->getOutputSocket(0);
-    bNode* editorsnode = getbNode();
-    NodeHueSat *storage = (NodeHueSat*)editorsnode->storage;
+	InputSocket *valueSocket = this->getInputSocket(0);
+	InputSocket *colourSocket = this->getInputSocket(1);
+	OutputSocket *outputSocket = this->getOutputSocket(0);
+	bNode* editorsnode = getbNode();
+	NodeHueSat *storage = (NodeHueSat*)editorsnode->storage;
 
-    if (colourSocket->isConnected() && outputSocket->isConnected()) {
-        ConvertRGBToHSVOperation * rgbToHSV = new ConvertRGBToHSVOperation();
-        ConvertHSVToRGBOperation * hsvToRGB = new ConvertHSVToRGBOperation();
-        ChangeHSVOperation *changeHSV = new ChangeHSVOperation();
-        MixBlendOperation * blend = new MixBlendOperation();
+	ConvertRGBToHSVOperation * rgbToHSV = new ConvertRGBToHSVOperation();
+	ConvertHSVToRGBOperation * hsvToRGB = new ConvertHSVToRGBOperation();
+	ChangeHSVOperation *changeHSV = new ChangeHSVOperation();
+	MixBlendOperation * blend = new MixBlendOperation();
 
-        colourSocket->relinkConnections(rgbToHSV->getInputSocket(0), true, 0, graph);
-        addLink(graph, rgbToHSV->getOutputSocket(), changeHSV->getInputSocket(0));
-        addLink(graph, changeHSV->getOutputSocket(), hsvToRGB->getInputSocket(0));
-        addLink(graph, hsvToRGB->getOutputSocket(), blend->getInputSocket(2));
-        addLink(graph, rgbToHSV->getInputSocket(0)->getConnection()->getFromSocket(), blend->getInputSocket(1));
-        valueSocket->relinkConnections(blend->getInputSocket(0), true, 0, graph);
-        outputSocket->relinkConnections(blend->getOutputSocket());
+	colourSocket->relinkConnections(rgbToHSV->getInputSocket(0), true, 0, graph);
+	addLink(graph, rgbToHSV->getOutputSocket(), changeHSV->getInputSocket(0));
+	addLink(graph, changeHSV->getOutputSocket(), hsvToRGB->getInputSocket(0));
+	addLink(graph, hsvToRGB->getOutputSocket(), blend->getInputSocket(2));
+	addLink(graph, rgbToHSV->getInputSocket(0)->getConnection()->getFromSocket(), blend->getInputSocket(1));
+	valueSocket->relinkConnections(blend->getInputSocket(0), true, 0, graph);
+	outputSocket->relinkConnections(blend->getOutputSocket());
 
-        changeHSV->setHue(storage->hue);
-        changeHSV->setSaturation(storage->sat);
-        changeHSV->setValue(storage->val);
+	changeHSV->setHue(storage->hue);
+	changeHSV->setSaturation(storage->sat);
+	changeHSV->setValue(storage->val);
 
-        blend->setResolutionInputSocketIndex(1);
+	blend->setResolutionInputSocketIndex(1);
 
-        graph->addOperation(rgbToHSV);
-        graph->addOperation(hsvToRGB);
-        graph->addOperation(changeHSV);
-        graph->addOperation(blend);
-    }
+	graph->addOperation(rgbToHSV);
+	graph->addOperation(hsvToRGB);
+	graph->addOperation(changeHSV);
+	graph->addOperation(blend);
 }
