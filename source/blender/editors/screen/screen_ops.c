0481a56416bf347e264b4f6b7a20b6ad6ab24d70
Jorge Rodriguez <bs.vino@gmail.com>
2012-06-06 01:34:54 +0000

    Allow activating the area move operator while pressing CTRL to facilitate the CTRL-blocking-merge bit.

--- source/blender/editors/screen/screen_ops.c
+++ source/blender/editors/screen/screen_ops.c
@@ -3723,7 +3723,7 @@ void ED_keymap_screen(wmKeyConfig *keyconf)
 	WM_keymap_verify_item(keymap, "SCREEN_OT_area_swap", EVT_ACTIONZONE_AREA, 0, KM_CTRL, 0);
 	WM_keymap_verify_item(keymap, "SCREEN_OT_region_scale", EVT_ACTIONZONE_REGION, 0, 0, 0);
 	/* area move after action zones */
-	WM_keymap_verify_item(keymap, "SCREEN_OT_area_move", LEFTMOUSE, KM_PRESS, 0, 0);
+	WM_keymap_verify_item(keymap, "SCREEN_OT_area_move", LEFTMOUSE, KM_PRESS, KM_ANY, 0);
 	
 	WM_keymap_verify_item(keymap, "SCREEN_OT_area_options", RIGHTMOUSE, KM_PRESS, 0, 0);
 	
