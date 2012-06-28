7d700a47c44e98fbe18d723b03768935e24fa552
Jason Wilkins <Jason.A.Wilkins@gmail.com>
2012-06-28 04:06:44 +0000

    forgot to add the color dictionary for the color readability commit

---
+++ source/blender/gpu/GPU_colors.h
@@ -0,0 +1,41 @@
+/*
+ * ***** BEGIN GPL LICENSE BLOCK *****
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * The Original Code is Copyright (C) 2012 Blender Foundation.
+ * All rights reserved.
+ *
+ * The Original Code is: all of this file.
+ *
+ * Contributor(s): Jason Wilkins
+ *
+ * ***** END GPL LICENSE BLOCK *****
+ */
+
+/** \file blender/gpu/GPU_colors.h
+ *  \ingroup gpu
+ */
+
+#define CPACK_BLACK   0x000000
+#define CPACK_WHITE   0xFFFFFF
+
+#define CPACK_RED     0xFF0000
+#define CPACK_GREEN   0x00FF00
+#define CPACK_BLUE    0x0000FF
+
+#define CPACK_YELLOW  0xFFFF00
+#define CPACK_CYAN    0x00FFFF
+#define CPACK_MAGENTA 0xFF00FF
