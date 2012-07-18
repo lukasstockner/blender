7ef71ae868fb1c73c22fb15325915d5561aa741b
Jason Wilkins <Jason.A.Wilkins@gmail.com>
2012-07-18 10:25:27 +0000

    Apparently structs need to be declared 'extern "C"' in order to link correctly (in MSVC2008 at least).

--- source/blender/gpu/GPU_object.h
+++ source/blender/gpu/GPU_object.h
@@ -3,19 +3,15 @@ typedef struct GPU_object_func
 	void (*gpuVertexPointer)(int size, int type, int stride, const void *pointer);
 	void (*gpuNormalPointer)(          int type, int stride, const void *pointer);
 	void (*gpuColorPointer )(int size, int type, int stride, const void *pointer);	
-
-
-
-
-
 } GPU_object_func;
 
-extern GPU_object_func gpugameobj;
 
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+extern GPU_object_func gpugameobj;
 
 void GPU_init_object_func(void);
 
