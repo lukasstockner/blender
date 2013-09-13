43ab2e5184af76949ebd333805989550b98c63f8
Daniel Stokes <kupomail@gmail.com>
2013-09-13 23:13:49 +0000

    Rewriting copy_object_lod to use BLI_duplicatelist.

--- source/blender/blenkernel/intern/object.c
+++ source/blender/blenkernel/intern/object.c
@@ -1308,27 +1308,10 @@ static void copy_object_pose(Object *obn, Object *ob)
 
 static void copy_object_lod(Object *obn, Object *ob)
 {
-	LodLevel *nlod, *lod;
+	BLI_duplicatelist(&obn->lodlevels, &ob->lodlevels);
 
-	obn->lodlevels.first = obn->lodlevels.last = NULL;
-
-	BKE_object_lod_add(obn);
-	nlod = obn->lodlevels.first;
-	nlod->distance = 0.0;
-	nlod->use_mat =nlod->use_mesh = 1;
-	nlod->source = obn;
-	obn->currentlod = nlod;
-
-	lod = ((LodLevel*)ob->lodlevels.first)->next;
-	for (; lod; lod = lod->next) {
-		BKE_object_lod_add(obn);
-		nlod = nlod->next;
-
-		nlod->distance = lod->distance;
-		nlod->source = lod->source;
-		nlod->use_mat = lod->use_mat;
-		nlod->use_mesh = lod->use_mesh;
-	}
+	((LodLevel*)obn->lodlevels.first)->source = obn;
+	obn->currentlod = (LodLevel*) obn->lodlevels.first;
 }
 
 bool BKE_object_pose_context_check(Object *ob)
