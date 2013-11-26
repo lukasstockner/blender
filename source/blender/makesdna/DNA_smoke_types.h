ff0296184c2803df0e4103d1b2308f00c3bd7ca4
Lukas Tönne <lukas.toenne@gmail.com>
2013-11-26 13:42:37 +0100

    Revert "Comment on smoke caches is deprecated." Thought this was referring to the ptcaches ListBase, but actually means the point_cache array, so i still important.
    This reverts commit 8f48294bd00b61bae93f375dc0a730cac7fb594e.

--- source/blender/makesdna/DNA_smoke_types.h
+++ source/blender/makesdna/DNA_smoke_types.h
@@ -130,6 +130,7 @@ typedef struct SmokeDomainSettings {
 	int cache_comp;
 	int cache_high_comp;
 
+	/* Smoke uses only one cache from now on (index [0]), but keeping the array for now for reading old files. */
 	struct PointCache *point_cache[2];	/* definition is in DNA_object_force.h */
 	struct ListBase ptcaches[2] DNA_DEPRECATED;
 	struct EffectorWeights *effector_weights;
