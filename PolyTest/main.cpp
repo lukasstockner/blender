49d78a3cc0fef6f27482461d326e1404981f89ba
Jonathan deWerd <jjoonathan@gmail.com>
2014-06-13 21:37:24 -0400

    Initial Commit

---
+++ PolyTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PolyTest
+//
+//  Created by Jonathan deWerd on 6/13/14.
+//  Copyright (c) 2014 a.b.c. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
