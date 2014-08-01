31c242fd0fa3931256a400de467cfea3d498ddc1
Grigory Revzin <revzingg@gmail.com>
2014-08-01 15:22:01 +0400

    Double triangle icons for Shape Key Move
    Shape Key Move now has proper icons for moving the shape keys to top or bottom of the list

--- release/scripts/startup/bl_ui/properties_data_mesh.py
+++ release/scripts/startup/bl_ui/properties_data_mesh.py
@@ -269,10 +269,10 @@ class DATA_PT_shape_keys(MeshButtonsPanel, Panel):
             col.separator()
 
             sub = col.column(align=True)
-            sub.operator("object.shape_key_move", icon='TRIA_UP', text="").type = 'TOP'
+            sub.operator("object.shape_key_move", icon='TRI_DOUBLE_UP', text="").type = 'TOP'
             sub.operator("object.shape_key_move", icon='TRIA_UP', text="").type = 'UP'
             sub.operator("object.shape_key_move", icon='TRIA_DOWN', text="").type = 'DOWN'
-            sub.operator("object.shape_key_move", icon='TRIA_DOWN', text="").type = 'BOTTOM'
+            sub.operator("object.shape_key_move", icon='TRI_DOUBLE_DOWN', text="").type = 'BOTTOM'
 
             split = layout.split(percentage=0.4)
             row = split.row()
