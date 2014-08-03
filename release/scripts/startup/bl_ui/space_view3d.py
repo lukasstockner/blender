2b67bf751041ce93952ba17fd1fa26f6798d33d2
Antony Riakiotakis <kalast@gmail.com>
2014-08-03 16:52:46 +0200

    Bring back extrude menu (was removed on earlier iteration of the code, no need to have this anymore)

--- release/scripts/startup/bl_ui/space_view3d.py
+++ release/scripts/startup/bl_ui/space_view3d.py
@@ -2188,6 +2188,46 @@ class VIEW3D_MT_edit_mesh_extrude(Menu):
             layout.operator("view3d.edit_mesh_extrude_move_normal", text="Region"),
         'REGION_VERT_NORMAL': lambda layout:
             layout.operator("view3d.edit_mesh_extrude_move_shrink_fatten", text="Region (Vertex Normals)"),
+    }
+
+    @staticmethod
+    def extrude_options(context):
+        mesh = context.object.data
+        select_mode = context.tool_settings.mesh_select_mode
+
+        menu = []
+        if mesh.total_face_sel:
+            menu += ['REGION', 'REGION_VERT_NORMAL', 'FACE']
+        if mesh.total_edge_sel and (select_mode[0] or select_mode[1]):
+            menu += ['EDGE']
+        if mesh.total_vert_sel and select_mode[0]:
+            menu += ['VERT']
+
+        # should never get here
+        return menu
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator_context = 'INVOKE_REGION_WIN'
+
+        for menu_id in self.extrude_options(context):
+            self._extrude_funcs[menu_id](layout)
+
+
+class VIEW3D_MT_edit_mesh_extrude_pie(Menu):
+    bl_label = "Extrude"
+
+    _extrude_funcs = {
+        'VERT': lambda layout:
+            layout.operator("mesh.extrude_vertices_move", text="Vertices Only"),
+        'EDGE': lambda layout:
+            layout.operator("mesh.extrude_edges_move", text="Edges Only"),
+        'FACE': lambda layout:
+            layout.operator("mesh.extrude_faces_move", text="Individual Faces"),
+        'REGION': lambda layout:
+            layout.operator("view3d.edit_mesh_extrude_move_normal", text="Region"),
+        'REGION_VERT_NORMAL': lambda layout:
+            layout.operator("view3d.edit_mesh_extrude_move_shrink_fatten", text="Region (Vertex Normals)"),
         'NULL': lambda layout:
             layout.separator(),
     }
