704d710ddc881162f4a9c67a08d503a6004d48d7
Campbell Barton <ideasman42@gmail.com>
2014-08-08 05:52:13 +1000

    remove unused import

--- release/scripts/templates_py/ui_pie_menu.py
+++ release/scripts/templates_py/ui_pie_menu.py
@@ -1,19 +1,19 @@
 import bpy
-from bpy.props import *
 from bpy.types import Menu
 
-#spawn an edit mode selection pie (run while object is in edit mode to get a valid output)
+# spawn an edit mode selection pie (run while object is in edit mode to get a valid output)
+
 
 class VIEW3D_PIE_template(Menu):
-    #label is displayed at the center of the pie menu.
+    # label is displayed at the center of the pie menu.
     bl_label = "Select Mode"
 
     def draw(self, context):
         layout = self.layout
 
         pie = layout.menu_pie()
-        #operator_enum will just spread all available options
-        #for the type enum of the operator on the pie
+        # operator_enum will just spread all available options
+        # for the type enum of the operator on the pie
         pie.operator_enum("mesh.select_mode", "type")
 
 
