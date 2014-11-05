f07a7a5a4bd3a94ad4fb61d3021abca3536ff7d7
Joshua Leung <aligorith@gmail.com>
2014-11-06 01:57:03 +1300

    GPencil Layer Lock - Does not prevent active frame from changing anymore
    Loosened the restrictions on when the current frame can change,
    by removing the restriction that the current frame on locked layers
    cannot change (we already have the Frame Locking functionality for
    that purpose instead). This makes it easier to animate (especially
    with PropEdit enabled, when you need to lock layers to prevent
    backgrounds and similar things from changing unexpectededly).

--- source/blender/blenkernel/intern/gpencil.c
+++ source/blender/blenkernel/intern/gpencil.c
@@ -384,7 +384,7 @@ bGPDframe *gpencil_layer_getframe(bGPDlayer *gpl, int cframe, short addnew)
 		/* do not allow any changes to layer's active frame if layer is locked from changes
 		 * or if the layer has been set to stay on the current frame
 		 */
-		if (gpl->flag & (GP_LAYER_LOCKED | GP_LAYER_FRAMELOCK))
+		if (gpl->flag & GP_LAYER_FRAMELOCK)
 			return gpf;
 		/* do not allow any changes to actframe if frame has painting tag attached to it */
 		if (gpf->flag & GP_FRAME_PAINT) 
