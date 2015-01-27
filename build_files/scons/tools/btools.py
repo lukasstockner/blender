04785c50701c03bdcdd88aed7769adf3ecebbe94
Joshua Leung <aligorith@gmail.com>
2015-01-27 14:37:50 +1300

    Import v2012.07 of OGDF into extern libs
    As a quick test, this imports OGDF into the source tree (later it could be removed
    and added as a prebuilt static lib instead). The intention here is to use it for
    drawing the debug graphs for the depsgraph.
    
    Only scons is supported right now.

--- build_files/scons/tools/btools.py
+++ build_files/scons/tools/btools.py
@@ -198,7 +198,7 @@ def validate_arguments(args, bc):
             'C_WARN', 'CC_WARN', 'CXX_WARN',
             'LLIBS', 'PLATFORM_LINKFLAGS', 'MACOSX_ARCHITECTURE', 'MACOSX_SDK', 'XCODE_CUR_VER', 'C_COMPILER_ID',
             'BF_CYCLES_CUDA_BINARIES_ARCH', 'BF_PROGRAM_LINKFLAGS', 'MACOSX_DEPLOYMENT_TARGET',
-            'WITH_BF_CYCLES_DEBUG', 'WITH_BF_CYCLES_LOGGING', 'WITH_BF_LEGACY_DEPSGRAPH'
+            'WITH_BF_CYCLES_DEBUG', 'WITH_BF_CYCLES_LOGGING', 'WITH_BF_LEGACY_DEPSGRAPH', 'WITH_BF_OGDF'
     ]
 
 
@@ -656,6 +656,7 @@ def read_opts(env, cfg, args):
         ('BF_PROGRAM_LINKFLAGS', 'Link flags applied only to final binaries (blender and blenderplayer, not makesrna/makesdna)', ''),
 
         (BoolVariable('WITH_BF_LEGACY_DEPSGRAPH', 'Build Blender with legacy dependency graph', True)),
+        (BoolVariable('WITH_BF_OGDF', 'Build Blender with OGDF for debugging depsgraph', True)),
     ) # end of opts.AddOptions()
 
     return localopts
