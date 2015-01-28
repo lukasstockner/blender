9386798b08c5ec63ff750180a9ffc4178e31d034
Sergey Sharybin <sergey.vfx@gmail.com>
2015-01-28 20:06:42 +0500

    Depsgraph: Move root pchan map to own file

--- source/blender/depsgraph/intern/depsgraph_build.h
+++ source/blender/depsgraph/intern/depsgraph_build.h
@@ -57,6 +57,7 @@ struct IDDepsNode;
 struct TimeSourceDepsNode;
 struct ComponentDepsNode;
 struct OperationDepsNode;
+struct RootPChanMap;
 
 struct DepsgraphNodeBuilder {
 	DepsgraphNodeBuilder(Main *bmain, Depsgraph *graph);
@@ -219,31 +220,6 @@ struct RNAPathKey
 
 struct DepsgraphRelationBuilder 
 {
-	struct RootPChanMap {
-		/* ctor and dtor - Create and free the internal map respectively */
-		RootPChanMap();
-		~RootPChanMap();
-
-		/* Debug contents of map */
-		void print_debug();
-
-		/* Add a mapping */
-		void add_bone(const char *bone, const char *root);
-
-		/* Check if there's a common root bone between two bones */
-		bool has_common_root(const char *bone1, const char *bone2);
-		
-	private:
-		/* The actual map:
-		 * - Keys are "strings" (const char *) - not dynamically allocated
-		 * - Values are "sets" (const char *) - not dynamically allocated
-		 *
-		 * We don't use the C++ maps here, as it's more convenient to use
-		 * Blender's GHash and be able to compare by-value instead of by-ref
-		 */
-		struct GHash *m_map;
-	};
-
 	DepsgraphRelationBuilder(Depsgraph *graph);
 	
 	template <typename KeyFrom, typename KeyTo>
--- source/blender/depsgraph/intern/depsgraph_build_relations.cpp
+++ source/blender/depsgraph/intern/depsgraph_build_relations.cpp
@@ -34,7 +34,6 @@
 
 extern "C" {
 #include "BLI_blenlib.h"
-#include "BLI_ghash.h"
 #include "BLI_string.h"
 #include "BLI_utildefines.h"
 
@@ -99,6 +98,8 @@ extern "C" {
 #include "depsgraph_intern.h"
 #include "depsgraph_types.h"
 
+#include "depsgraph_util_pchanmap.h"
+
 #include "stubs.h" // XXX: REMOVE THIS INCLUDE ONCE DEPSGRAPH REFACTOR PROJECT IS DONE!!!
 
 namespace {
@@ -153,103 +154,6 @@ bool modifier_check_depends_on_time(Object *ob, ModifierData *md)
 }
 
 }  /* namespace */
-
-/* ***************** */
-/* Pose Channels "Root" Map */
-
-DepsgraphRelationBuilder::RootPChanMap::RootPChanMap()
-{
-	/* just create empty map */
-	m_map = BLI_ghash_str_new("RootPChanMap");
-}
-
-static void free_rootpchanmap_valueset(void *val)
-{
-	/* just need to free the set itself - the names stored are all references */
-	GSet *values = (GSet *)val;
-	BLI_gset_free(values, NULL);
-}
-
-DepsgraphRelationBuilder::RootPChanMap::~RootPChanMap()
-{
-	/* free the map, and all the value sets */
-	BLI_ghash_free(m_map, NULL, free_rootpchanmap_valueset);
-}
-
-/* Debug contents of map */
-void DepsgraphRelationBuilder::RootPChanMap::print_debug()
-{
-	GHashIterator it1;
-	GSetIterator it2;
-	
-	printf("Root PChan Map:\n");
-	GHASH_ITER(it1, m_map) {
-		const char *item = (const char *)BLI_ghashIterator_getKey(&it1);
-		GSet *values = (GSet *)BLI_ghashIterator_getValue(&it1);
-
-		printf("  %s : { ", item);
-		GSET_ITER(it2, values) {
-			const char *val = (const char *)BLI_gsetIterator_getKey(&it2);
-			printf("%s, ", val);
-		}
-		printf("}\n");
-	}
-}
-
-/* Add a mapping */
-void DepsgraphRelationBuilder::RootPChanMap::add_bone(const char *bone, const char *root)
-{
-	if (BLI_ghash_haskey(m_map, bone)) {
-		/* add new entry, but only add the root if it doesn't already exist in there */
-		GSet *values = (GSet *)BLI_ghash_lookup(m_map, bone);
-		BLI_gset_add(values, (void *)root);
-	}
-	else {
-		/* create new set and mapping */
-		GSet *values = BLI_gset_new(BLI_ghashutil_strhash_p, BLI_ghashutil_strcmp, "RootPChanMap Value Set");
-		BLI_ghash_insert(m_map, (void *)bone, (void *)values);
-
-		/* add new entry now */
-		BLI_gset_insert(values, (void *)root);
-	}
-}
-
-/* Check if there's a common root bone between two bones */
-bool DepsgraphRelationBuilder::RootPChanMap::has_common_root(const char *bone1, const char *bone2)
-{
-	/* Ensure that both are in the map... */
-	if (BLI_ghash_haskey(m_map, bone1) == false) {
-		//fprintf("RootPChanMap: bone1 '%s' not found (%s => %s)\n", bone1, bone1, bone2);
-		//print_debug();
-		return false;
-	}
-
-	if (BLI_ghash_haskey(m_map, bone2) == false) {
-		//fprintf("RootPChanMap: bone2 '%s' not found (%s => %s)\n", bone2, bone1, bone2);
-		//print_debug();
-		return false;
-	}
-
-	GSet *bone1_roots = (GSet *)BLI_ghash_lookup(m_map, (void *)bone1);
-	GSet *bone2_roots = (GSet *)BLI_ghash_lookup(m_map, (void *)bone2);
-
-	GSetIterator it1, it2;
-	GSET_ITER(it1, bone1_roots) {
-		GSET_ITER(it2, bone2_roots) {
-			const char *v1 = (const char *)BLI_gsetIterator_getKey(&it1);
-			const char *v2 = (const char *)BLI_gsetIterator_getKey(&it2);
-
-			if (strcmp(v1, v2) == 0) {
-				//fprintf("RootPchanMap: %s in common for %s => %s\n", v1, bone1, bone2);
-				return true;
-			}
-		}
-	}
-
-	//fprintf("RootPChanMap: No common root found (%s => %s)\n", bone1, bone2);
-	return false;
-}
-
 
 /* ***************** */
 /* Relations Builder */
