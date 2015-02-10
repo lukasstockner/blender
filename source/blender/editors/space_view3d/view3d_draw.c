bdfd9ba38abcb588eafb6413ed24ad5c310a44cb
Antony Riakiotakis <kalast@gmail.com>
2015-02-10 12:57:11 +0100

    Cleanup leftover code from WIP renderer

--- source/blender/editors/space_view3d/view3d_draw.c
+++ source/blender/editors/space_view3d/view3d_draw.c
@@ -95,7 +95,6 @@
 #include "GPU_material.h"
 #include "GPU_extensions.h"
 #include "GPU_compositing.h"
-#include "GPU_renderer.h"
 
 #include "view3d_intern.h"  /* own include */
 
@@ -2799,9 +2798,6 @@ static void view3d_draw_objects(
 		if (v3d->zbuf) glEnable(GL_DEPTH_TEST);
 	}
 
-	/* draw sorted materials in the gpu renderer */
-	if (v3d->gpu_material.first)         GPU_renderer_material_draw(&v3d->gpu_material);
-	
 	/* transp and X-ray afterdraw stuff */
 	if (v3d->afterdraw_transp.first)     view3d_draw_transp(scene, ar, v3d);
 	if (v3d->afterdraw_xray.first)       view3d_draw_xray(scene, ar, v3d, true);
