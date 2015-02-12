ccd3fb8ea7a0e8d4df6a5611b126ee2e6d223bbf
Antony Riakiotakis <kalast@gmail.com>
2015-02-12 18:15:15 +0100

    Also write DOF if it exists.

--- source/blender/blenloader/intern/writefile.c
+++ source/blender/blenloader/intern/writefile.c
@@ -2685,6 +2685,8 @@ static void write_screens(WriteData *wd, ListBase *scrbase)
 
 					if (v3d->fx_settings.ssao)
 						writestruct(wd, DATA, "GPUSSAOSettings", 1, v3d->fx_settings.ssao);
+					if (v3d->fx_settings.dof)
+						writestruct(wd, DATA, "GPUDOFSettings", 1, v3d->fx_settings.ssao);
 				}
 				else if (sl->spacetype==SPACE_IPO) {
 					SpaceIpo *sipo= (SpaceIpo *)sl;
