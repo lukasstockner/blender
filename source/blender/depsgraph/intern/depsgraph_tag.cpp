69aabd33af2bed09e4c73f6a204f05ddd705bd74
Sergey Sharybin <sergey.vfx@gmail.com>
2015-02-13 14:15:56 +0500

    Depsgraph: Fix crash when trying to tag NULL for recalc
    I would say it's better to be avoid to tag NULL for update, but old
    depsgraph allowed that and nobody id check for it.

--- source/blender/depsgraph/intern/depsgraph_tag.cpp
+++ source/blender/depsgraph/intern/depsgraph_tag.cpp
@@ -136,6 +136,10 @@ void DEG_id_tag_update(ID *id, short flag)
 
 void DEG_id_tag_update_ex(Main *bmain, ID *id, short flag)
 {
+	if(id == NULL) {
+		/* Ideally should not happen, but old depsgraph allowed this. */
+		return;
+	}
 	DEG_DEBUG_PRINTF("%s: id=%s flag=%d\n", __func__, id->name, flag);
 	lib_id_recalc_tag_flag(bmain, id, flag);
 	for (Scene *scene = (Scene *)bmain->scene.first;
