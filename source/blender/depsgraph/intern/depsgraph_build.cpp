006dccd87b7ab72e680553f8133e9a60282f5285
Sergey Sharybin <sergey.vfx@gmail.com>
2015-03-16 18:25:02 +0500

    Depsgraph: Code cleanup, move transitive reduction to own file
    It might actually be gone in the future since it causes issues
    with threaded evaluation order.

--- source/blender/depsgraph/intern/depsgraph_build.cpp
+++ source/blender/depsgraph/intern/depsgraph_build.cpp
@@ -97,6 +97,7 @@ extern "C" {
 #include "depsgraph_intern.h"
 
 #include "depsgraph_util_cycle.h"
+#include "depsgraph_util_transitive.h"
 
 /* ****************** */
 /* External Build API */
@@ -174,77 +175,6 @@ string deg_fcurve_id_name(const FCurve *fcu)
 	sprintf(index_buf, "[%d]", fcu->array_index);
 
 	return string(fcu->rna_path) + index_buf;
-}
-
-/* -------------------------------------------------- */
-
-/* performs a transitive reduction to remove redundant relations
- * http://en.wikipedia.org/wiki/Transitive_reduction
- *
- * XXX The current implementation is somewhat naive and has O(V*E) worst case runtime.
- * A more optimized algorithm can be implemented later, e.g.
- *
- * http://www.sciencedirect.com/science/article/pii/0304397588900321/pdf?md5=3391e309b708b6f9cdedcd08f84f4afc&pid=1-s2.0-0304397588900321-main.pdf
- *
- * Care has to be taken to make sure the algorithm can handle the cyclic case too!
- * (unless we can to prevent this case early on)
- */
-
-enum {
-	OP_VISITED = 1,
-	OP_REACHABLE = 2,
-};
-
-static void deg_graph_tag_paths_recursive(DepsNode *node)
-{
-	if (node->done & OP_VISITED)
-		return;
-	node->done |= OP_VISITED;
-
-	for (OperationDepsNode::Relations::const_iterator it = node->inlinks.begin(); it != node->inlinks.end(); ++it) {
-		DepsRelation *rel = *it;
-
-		deg_graph_tag_paths_recursive(rel->from);
-		/* do this only in inlinks loop, so the target node does not get flagged! */
-		rel->from->done |= OP_REACHABLE;
-	}
-}
-
-static void deg_graph_transitive_reduction(Depsgraph *graph)
-{
-	for (Depsgraph::OperationNodes::const_iterator it_target = graph->operations.begin(); it_target != graph->operations.end(); ++it_target) {
-		OperationDepsNode *target = *it_target;
-
-		/* clear tags */
-		for (Depsgraph::OperationNodes::const_iterator it = graph->operations.begin(); it != graph->operations.end(); ++it) {
-			OperationDepsNode *node = *it;
-			node->done = 0;
-		}
-
-		/* mark nodes from which we can reach the target
-		 * start with children, so the target node and direct children are not flagged
-		 */
-		target->done |= OP_VISITED;
-		for (OperationDepsNode::Relations::const_iterator it = target->inlinks.begin(); it != target->inlinks.end(); ++it) {
-			DepsRelation *rel = *it;
-
-			deg_graph_tag_paths_recursive(rel->from);
-		}
-
-		/* remove redundant paths to the target */
-		for (DepsNode::Relations::const_iterator it_rel = target->inlinks.begin(); it_rel != target->inlinks.end();) {
-			DepsRelation *rel = *it_rel;
-			++it_rel; /* increment in advance, so we can safely remove the relation */
-
-			if (rel->from->type == DEPSNODE_TYPE_TIMESOURCE) {
-				/* HACK: time source nodes don't get "done" flag set/cleared */
-				// TODO: there will be other types in future, so iterators above need modifying
-			}
-			else if (rel->from->done & OP_REACHABLE) {
-				OBJECT_GUARDED_DELETE(rel, DepsRelation);
-			}
-		}
-	}
 }
 
 static void deg_graph_build_finalize(Depsgraph *graph)
