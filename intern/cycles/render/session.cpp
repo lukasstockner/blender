2722346eae86c74f456091d83d02f3dbebae20e0
varunsundar08 <varunsundar08@gmail.com>
2015-04-28 20:19:39 +0530

    Bug fix : Don't display sample count in progress bar when using split kernel

--- intern/cycles/render/session.cpp
+++ intern/cycles/render/session.cpp
@@ -836,13 +836,17 @@ void Session::update_status_time(bool show_pause, bool show_done)
 	string status, substatus;
 
 	if(!params.progressive) {
+
 		bool is_gpu = params.device.type == DEVICE_CUDA || params.device.type == DEVICE_OPENCL;
 		bool is_multidevice = params.device.multi_devices.size() > 1;
 		bool is_cpu = params.device.type == DEVICE_CPU;
 
 		substatus = string_printf("Path Tracing Tile %d/%d", tile, num_tiles);
 
-		if((is_gpu && !is_multidevice) || (is_cpu && num_tiles == 1)) {
+		if(((is_gpu && !is_multidevice) || (is_cpu && num_tiles == 1)) && !device->use_split_kernel) {
+			/* When using split-kernel (OpenCL) each thread in a tile will be working on a different
+			 * sample. Can't display sample number when device uses split-kernel
+			 */
 			/* when rendering on GPU multithreading happens within single tile, as in
 			 * tiles are handling sequentially and in this case we could display
 			 * currently rendering sample number
