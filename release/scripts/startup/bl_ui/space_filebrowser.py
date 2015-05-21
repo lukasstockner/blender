a5c580c9e49a4ad96af4fce7d82f4fa262e5fddb
Bastien Montagne <montagne29@wanadoo.fr>
2015-05-21 15:20:53 +0200

    Some tweaking to UI, try to make filebrowser header a bit more compact...

--- release/scripts/startup/bl_ui/space_filebrowser.py
+++ release/scripts/startup/bl_ui/space_filebrowser.py
@@ -40,12 +40,10 @@ class FILEBROWSER_HT_header(Header):
         row.operator("file.parent", text="", icon='FILE_PARENT')
         row.operator("file.refresh", text="", icon='FILE_REFRESH')
 
-        row = layout.row()
-        row.separator()
-
-        row = layout.row(align=True)
+        layout.separator()
         layout.operator_context = 'EXEC_DEFAULT'
-        row.operator("file.directory_new", icon='NEWFOLDER')
+        layout.operator("file.directory_new", icon='NEWFOLDER', text="")
+        layout.separator()
 
         layout.operator_context = 'INVOKE_DEFAULT'
         params = st.params
@@ -54,13 +52,14 @@ class FILEBROWSER_HT_header(Header):
         if params:
             is_lib_browser = params.use_library_browsing
 
+            layout.prop(params, "recursion_level", text="")
+
             layout.prop(params, "display_type", expand=True, text="")
 
             layout.prop(params, "thumbnail_size", text="")
 
             layout.prop(params, "sort_method", expand=True, text="")
 
-            layout.prop(params, "recursion_level")
             layout.prop(params, "show_hidden", text="", icon='FILE_HIDDEN')
             layout.prop(params, "use_filter", text="", icon='FILTER')
 
