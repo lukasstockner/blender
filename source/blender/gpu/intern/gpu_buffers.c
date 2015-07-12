43bf4a0f511bcc479112690261e82230b2e012a1
Antony Riakiotakis <kalast@gmail.com>
2015-07-12 16:40:47 +0200

    Bah, merge change that was left uncommitted

--- source/blender/gpu/intern/gpu_buffers.c
+++ source/blender/gpu/intern/gpu_buffers.c
@@ -465,10 +465,6 @@ static GPUBuffer *gpu_try_realloc(GPUBufferPool *pool, GPUBuffer *buffer, int si
 	return buffer;
 }
 
-typedef void (*GPUBufferCopyFunc)(
-        DerivedMesh *dm, float *varray, int *index,
-        const int *mat_orig_to_new, const void *user_data);
-
 static GPUBuffer *gpu_buffer_setup(DerivedMesh *dm, GPUDrawObject *object,
                                    int type, void *user)
 {
