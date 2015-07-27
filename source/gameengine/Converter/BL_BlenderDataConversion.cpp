354c663f66d151d75bda9d082a2303935172c57d
Antony Riakiotakis <kalast@gmail.com>
2015-07-27 16:32:37 +0200

    Only create the tessface layer if it does not exist.

--- source/gameengine/Converter/BL_BlenderDataConversion.cpp
+++ source/gameengine/Converter/BL_BlenderDataConversion.cpp
@@ -961,12 +961,14 @@ RAS_MeshObject* BL_ConvertMesh(Mesh* mesh, Object* blenderobj, KX_Scene* scene, 
 
 	/* needs to be rewritten for loopdata */
 	if (tface) {
-		bool generate_data = false;
-		if (CustomData_get_layer_index(&dm->loopData, CD_TANGENT) == -1) {
-			DM_add_tangent_layer(dm);
-			generate_data = true;
+		if (CustomData_get_layer_index(&dm->faceData, CD_TANGENT) == -1) {
+			bool generate_data = false;
+			if (CustomData_get_layer_index(&dm->loopData, CD_TANGENT) == -1) {
+				DM_add_tangent_layer(dm);
+				generate_data = true;
+			}
+			DM_generate_tangent_tessface_data(dm, generate_data);
 		}
-		DM_generate_tangent_tessface_data(dm, generate_data);
 		tangent = (float(*)[4])dm->getTessFaceDataArray(dm, CD_TANGENT);
 	}
 
