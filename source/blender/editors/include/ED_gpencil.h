0fb97b6b5973bce4b76d5619c1b5a1a57955891a
Joshua Leung <aligorith@gmail.com>
2015-10-24 01:47:32 +1300

    GPencil Dopesheet Mode: Copy and Paste for Keyframes works again
    For the first time since the 2.4x series, the copy and paste functionality for
    Grease Pencil keyframes finally works again. Not only that, but it's now been
    upgraded to include some of the new goodness supported by the 2.7x keyframe
    copy and paste tools.

--- source/blender/editors/include/ED_gpencil.h
+++ source/blender/editors/include/ED_gpencil.h
@@ -41,6 +41,7 @@ struct bGPdata;
 struct bGPDlayer;
 struct bGPDframe;
 struct bGPDstroke;
+struct bAnimContext;
 struct PointerRNA;
 struct wmWindowManager;
 struct wmKeyConfig;
@@ -123,11 +124,11 @@ void ED_gplayer_frames_keytype_set(struct bGPDlayer *gpl, short type);
 
 void  ED_gplayer_snap_frames(struct bGPDlayer *gpl, struct Scene *scene, short mode);
 
-#if 0
-void free_gpcopybuf(void);
-void copy_gpdata(void);
-void paste_gpdata(void);
+void ED_gpencil_anim_copybuf_free(void);
+bool ED_gpencil_anim_copybuf_copy(struct bAnimContext *ac);
+bool ED_gpencil_anim_copybuf_paste(struct bAnimContext *ac, const short copy_mode);
 
+#if 0
 void mirror_gplayer_frames(struct bGPDlayer *gpl, short mode);
 #endif
 
